5bac3b10c9d4e4656296189a40b2473b60e59ec5
Diegovano <36131808+Diegovano@users.noreply.github.com>
2020-05-26 19:35:31 +0100

    Changed CARTO Map to use dark map without labels

--- astropi/map.js
+++ astropi/map.js
@@ -13,7 +13,7 @@ L.tileLayer(
     }).addTo(mapbox);
 
 L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     	subdomains: 'abcd',
     	maxZoom: 19
